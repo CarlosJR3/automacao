d27d038f679cb3eec845f700fb56722125142d2b
José carlos Junior <carlos.fer.junior@gmail.com>
2026-03-16 11:04:34 -0300

    começo

---
+++ automacao.py
@@ -0,0 +1,18 @@
+# bibliotecas = pacotes de código
+# pip install pyautoqui
+
+
+import pyautogui 
+
+# pyautogui.clinck -> clica (clink)
+# pyautogui.write -> escreve um texto(write)
+# pyautogui.press -> aperta uma tecla(press)
+# pyautogui.hotkey -> aperta um atalho(hotkey)
+# Passo a passo do seu programa
+# Passo 1 : Entrar no sistema da empresa  
+# Passo 2 : Fazer login
+# Passo 3 : Abrir a base de dados
+# Passo 4 : Cadastar 1 produto
+# Passo 5 : Repetir o passo 4 até acabar a lista de produtos
+ 
+
